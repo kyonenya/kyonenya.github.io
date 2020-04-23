b591cb78976ed9ff3e93119d337a8bba071fe0cd
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-04-23 13:51:16 +0900

    検索 末尾省略…を実装

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict'
-{
+{	
 	// 定数宣言
 	let html = ""; // htmlタグ
 	const hashtag = [];
@@ -22,8 +22,10 @@
 /* -- JSONデータ取得開始 -- */
 $(function(){
 $.getJSON("data.json", function(data) {
+	// セレクター
+	const input = document.getElementById('input_searchWord');
 
-	// 古い順にソート
+	// 古い順に逆順ソート
 /*	data.sort(function(a, b) {
 		if (a.date > b.date) {
 			return 1;
@@ -31,11 +33,10 @@ $.getJSON("data.json", function(data) {
 			return -1;
 		}
 	})
-	console.log(data);
 */
 
 	for (var i = 0; i < data.length; i=i+1) {
-		// プレーンテキスト生成
+		// プレーンテキストを生成して配列に格納
 		plainTexts[i] = data[i].text.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
 
 		// ハッシュタグをli要素として生成して結合
@@ -49,99 +50,80 @@ $.getJSON("data.json", function(data) {
 			</li>`;
 		};
 
-
 	// 記事一覧リストでは表示長文の表示文字数を制限する
-		// 記事一覧の要約テキスト用に、dataオブジェクトに新しいプロパティ'shortText'を追加
 		// まずa, hr, blockquoteタグを削除、それから複数段落を一つの段落へと統合
 		data[i].shortText = data[i].text.replace(/<\/?a.*?>|<hr>|<\/?blockquote>/g, '').replace(/<\/p><p>/g, ''); 
-		// console.log(data[i].shortText);
 
 		// 長文なら省略表示をして「…」を追加
 		if (data[i].text.length > shortTextLength) {
-			postTexts[i] = `${data[i].shortText.substr(0, shortTextLength)}...`;
+			postTexts[i] = `${data[i].shortText.substr(0, shortTextLength)}…`;
 		} else {
 			postTexts[i] = data[i].shortText;
 		};
 
-		// 【検索機能試作】
-/*
-			let searchWordIndex = data[i].text.indexOf('意味');
-			console.log(searchWordIndex);
-			if (searchWordIndex === -1) {
-				continue; 
-			}
-			if (searchWordIndex <= 16) {
-				searchWordIndex = 16; 
-			}
-			postTexts[i] = `…${data[i].text.substr(searchWordIndex - 16, 42)}…`;
-*/
 		// 記事一覧ページのHTMLタグを積算
 		html += htmlComb(i);
 
-	} // for (1 < i < data.length) {...
+	} // for() {...
 
-
-// --リアルタイム検索
-	const form = document.getElementById('form_searchWord');
+/* ---------------------------------
+	リアルタイム検索 */
 	
 	// 検索関数
 	const searchWord = () => {
-		const searchWord = form.value; // 検索ボックスに入力された値
-		const resultLength = 42; // 検索結果の全文字数
-		const beforeLength = 16; // マッチした検索語句の何文字前から先読みするか
-
-		for (var i = 0; i < data.length; i = i + 1) {
+		const searchWord = input.value; // 検索ボックスに入力された値
+		// 全件ループ開始
+		for (var i = 0; i < data.length; i=i+1) {
 			const li = document.querySelectorAll('.bl_posts_item');
 			const li_text = document.querySelectorAll('.bl_posts_item .bl_text');
 			let searchWordIndex = plainTexts[i].indexOf(searchWord);
 			let resultText = '…';
 
-			// マッチしたら
+			// 表示調整用
+			const beforeLength = 16; // 先読み、マッチした検索語句の何文字前から？
+			const afterLength = 26; // 後読み
+			const resultLength = beforeLength + searchWord.length + afterLength; // 先読み＋検索語句＋後読み＝結果文字列
+		
+			// マッチしたときは、
 			if (searchWordIndex != -1) {
-				li[i].classList.remove('hp_hidden'); // 表示
-				
-				// 検索語句が先頭に近すぎたら
+				li[i].classList.remove('hp_hidden'); // 表示。				
+				// 検索語句が先頭に近すぎたら、
 				if (searchWordIndex <= beforeLength) {
-					searchWordIndex = beforeLength; // 冒頭から表示する
-					resultText = '';
-				}
-				
+					searchWordIndex = beforeLength; // 冒頭から表示して、
+					resultText = ''; // 冒頭の'…'を削除。
+				}				
 				// 結果表示用の文字列
-				resultText += plainTexts[i].substr(searchWordIndex - beforeLength, searchWord.length + resultLength) // 先読み文字列［マッチした語句］後続文字列
-
-				// 検索語句が末尾に近かったら
-/*				console.log(resultText.length < resultLength);
-
-				if (resultText.length > resultLength) {
-					resultText += '…';
+				resultText += plainTexts[i].substr(searchWordIndex - beforeLength, resultLength)
+				// 検索語句が末尾より十分遠ければ、
+				const lastIndex = searchWordIndex + searchWord.length + afterLength;
+				if (lastIndex < plainTexts[i].length) {
+					resultText += '…'; // 末尾に'…'を追加。
 				}
-*/				
+				// 検索語句をハイライト表示する
 				resultText = resultText.replace(new RegExp(searchWord, "g"), `<span class="hp_highlight">${searchWord}<\/span>`); // 変数を使って複数置換させる
-				
-				// 検索語句の周辺の文字列を表示
+				// DOM要素として追加
 				li_text[i].innerHTML = `<p>${resultText}</p>`;
-
+			
 			} else {
-			// マッチしなかったときは
-				li[i].classList.add('hp_hidden'); // 非表示に
+			// マッチしなかったときは、
+				li[i].classList.add('hp_hidden'); // 非表示に。
 			}
 			
-			// 検索フォームが空になったら
+			// 検索フォームが空になったら、
 			if (searchWord === '') {
-				li_text[i].innerHTML = postTexts[i] // 元のテキストに戻す
+				li_text[i].innerHTML = postTexts[i] // 元のテキストに戻す。
 			};
 
-		}; // for...
-
+		}; // for(){...
 	}; // searchWord()...
 
 	// 文字入力されるたびに検索実行
-	form.addEventListener('input', () => {
+	input.addEventListener('input', () => {
 		searchWord();
 	});
 
-//--
-
+/* ---------------------------------
+	HTML生成 */
 	if (id == 0) {
 		$("#postlistWrapper").append(html);
 	} else {
@@ -150,8 +132,11 @@ $.getJSON("data.json", function(data) {
 		$('title').html(`placet experiri :: ${id}`);
 		document.getElementById('description').content = data[data.length - id].text.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').substr(0, 140); // HTMLタグを削除して先頭140文字をとる
 		document.getElementById('ogDescription').content = data[data.length - id].text.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').substr(0, 140); // 同上、OGPはJS未対応なので無駄だけど
+		input.classList.add('hp_hidden'); // 検索フォームを非表示に
 	};
 
+/* ---------------------------------
+	HTMLテンプレート */
 	// 記事リスト（HTMLタグ生成関数）邪魔なので末尾に。関数の巻き上げ。
 	function htmlComb(i) {
 		return `<li class="bl_posts_item">
@@ -198,5 +183,4 @@ $.getJSON("data.json", function(data) {
 
 }); // $.getJSON(){...
 }); // $(function(){...
-
-}
+} // {...
