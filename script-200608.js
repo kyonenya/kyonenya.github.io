ec03f6edae99c7de4ceb9f00ab79498b6f747f45
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-07-02 00:20:16 +0900

    タグフィルター時のリアルタイム検索がずれるバグを修正。onView配列をmapではなくreduceで生成

--- script-200608.js
+++ script-200608.js
@@ -72,6 +72,7 @@ fetch(jsonPath)
 /* ---------------------------------
 	記事一覧ページ生成 */
 	// この配列に登録されたidだけが記事リストに表示される
+	/*
 	status.onView = data.map((eachData) => {	
 		const tagExists = eachData.tags.includes(status.tag);	// タグ検索にヒットしたか
 		// タグ検索がOFFか、またはタグ検索にヒットしているならば、
@@ -79,6 +80,19 @@ fetch(jsonPath)
 			return eachData.id;	// 表示リストにidを登録。
 		}
 	});
+	*/
+
+	status.onView = data.reduce((accumulator, eachData) => {
+		const tagExists = eachData.tags.includes(status.tag);	// タグ検索にヒットしたか
+		// タグ検索がOFFか、またはタグ検索にヒットしているならば、
+		if (status.tag == null || tagExists) {
+			accumulator.push(eachData.id);	// 表示リストにidを登録。
+		}
+		return accumulator;
+	}, []);	// 初期値は空の配列
+		// mapは入力と同数の配列を出力してしまうため、ループをスキップできず、nullで埋められてしまう。
+		// filterは入力した配列、つまりdata配列が丸ごと出力されてしまうので却下。
+	// console.log(status.onView);
 
 	const postlist = {
 		html: `<ul class="bl_posts">${
@@ -103,6 +117,7 @@ fetch(jsonPath)
 		renderHTML(postlist);
 	};
 
+	//console.log(document.querySelector('.bl_posts_summary[data-id="10"]').innerText);
 
 /* ---------------------------------
 	個別記事ページ生成 */
@@ -125,9 +140,9 @@ fetch(jsonPath)
 		renderHTML(article);	// ページ生成
 	};
 	
+	
 	/* ---------------------------------
 		リアルタイム検索 */
-	
 	/* 検索関数 ---------- */
 	const realTimeSearch = () => {
 
@@ -135,16 +150,19 @@ fetch(jsonPath)
 		const searchWord = document.querySelector('.el_search_form').value;
 		
 		// 全件ループ開始
-		for (var i = 0; i < data.length; i=i+1) {
-			const li = document.querySelectorAll('.bl_posts_item');
-			const li_text = document.querySelectorAll('.bl_posts_summary');
-
-			// 変数
+//		for (var i = 0; i < data.length; i=i+1) {
+		for (const eachId of status.onView) {
+			//const li = document.querySelectorAll('.bl_posts_item');
+			//const li_text = document.querySelectorAll('.bl_posts_summary');
+			const i = data.length - eachId
+			const li = document.querySelector(`.bl_posts_item[data-id="${eachId}"]`);
+			const li_text = document.querySelector(`.bl_posts_summary[data-id="${eachId}"]`);
+			
 			let searchWordIndex = data[i].plainText.indexOf(searchWord);
 			let searchWordIndex_title = data[i].title.indexOf(searchWord);	// タイトル簡易検索
 			let searchWordIndex_hashtags = data[i].hashtags.indexOf(searchWord);	// ハッシュタグ簡易検索
 			let resultText = '…';
-
+			
 			// 表示調整用
 			const resultLength = 41;	// 検索結果に表示したい文字数は？
 			const beforeLength = 15;	// 先読み、マッチした検索語句の何文字前から表示したい？
@@ -152,7 +170,8 @@ fetch(jsonPath)
 			
 			// マッチしたときは（本文・タイトル・タグのいずれかに）
 			if (searchWordIndex != -1 || searchWordIndex_title != -1 || searchWordIndex_hashtags != -1) {
-				li[i].classList.remove('hp_hidden');	// 表示。
+				li.classList.remove('hp_hidden');	// 表示。
+				
 				// 検索語句が先頭に近すぎたら、
 				if (searchWordIndex <= beforeLength) {
 					searchWordIndex = beforeLength;	// 冒頭から表示して、
@@ -168,16 +187,16 @@ fetch(jsonPath)
 				// 検索語句をハイライト表示する
 				resultText = resultText.replace(new RegExp(searchWord, "g"), `<span class="hp_highlight">${searchWord}<\/span>`);	// （変数を使って複数置換させる方法）
 				// DOM要素として追加
-				li_text[i].innerHTML = `<p>${resultText}</p>`;
-			
+				li_text.innerHTML = `<p>${resultText}</p>`;
+				
 			} else {
 			// マッチしなかったときは、
-				li[i].classList.add('hp_hidden');	// 非表示に。
+				li.classList.add('hp_hidden');	// 非表示に。
 			}
 			
 			// 検索フォームが空になったら、
 			if (searchWord === '') {
-				li_text[i].innerHTML = data[i].postText	// 元のテキストに戻す。
+				li_text.innerHTML = data[i].postText	// 元のテキストに戻す。
 			};
 
 		};	// for(){...
@@ -195,7 +214,7 @@ fetch(jsonPath)
 	// 記事一覧リスト
 	function html_postlist(i) {
 		return `
-		<li class="bl_posts_item">
+		<li class="bl_posts_item" data-id=${data.length - i}>
 			<a href="?id=${data.length - i}">
 				<header class="bl_posts_header">
 					<time class="bl_posts_date" datetime="${moment(data[i].date).format("YYYY-MM-DD HH:mm")}">${moment(data[i].date).format("YYYY-MM-DD")}
@@ -204,7 +223,7 @@ fetch(jsonPath)
 				<h2 class="bl_posts_title">
 					${data[i].title}
 				</h2>
-				<div class="bl_posts_summary">
+				<div class="bl_posts_summary" data-id=${data.length - i}>
 					<p>${data[i].postText}</p>
 				</div>
 			</a>
