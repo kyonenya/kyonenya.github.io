5a175b0d362138244a9df9c92e733952eff08116
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-07-04 21:41:50 +0900

    リアルタイム検索の変数を整理
    リファクタリングは難航

--- script.js
+++ script.js
@@ -29,7 +29,6 @@
 	
 	// 実行
 	const status = getUrlQueries();
-	//status.onView = [];
 
 /* JSONデータ取得開始 --------------------  */
 fetch(jsonPath)
@@ -136,47 +135,49 @@ fetch(jsonPath)
 	const realTimeSearch = () => {
 
 		// 検索ボックスに入力された値
-		const searchWord = document.querySelector('.el_search_form').value;
+		const word = document.querySelector('.el_search_form').value;
 		
 		// 全件ループ開始
 		for (const eachData of data) {
-		
-			if (eachData.isVisible === false) {	// 記事一覧に表示されてなければ、
-				continue;	// スキップして次の記事へ。
-			};
-		
-			// const i = data.length - eachData.id;
+
 			const li = document.querySelector(`.bl_posts_item[data-id="${eachData.id}"]`);
 			const li_text = document.querySelector(`.bl_posts_summary[data-id="${eachData.id}"]`);
 			
-			let searchWordIndex = eachData.plainText.indexOf(searchWord);
-			let searchWordIndex_title = eachData.title.indexOf(searchWord);	// タイトル簡易検索
-			let searchWordIndex_hashtags = eachData.hashtags.indexOf(searchWord);	// ハッシュタグ簡易検索
+			// 記事一覧に表示されてなければ、
+			if (eachData.isVisible === false) {
+				continue;	// スキップして次のループへ。
+			};
+			
+			let wordIndex = eachData.plainText.indexOf(word);
+			const isMatched_title = eachData.title.includes(word);	// タイトル簡易検索
+			let isMatched_hashtags = eachData.hashtags.includes(word);	// ハッシュタグ簡易検索
 			let resultText = '…';
 			
 			// 表示調整用
 			const resultLength = 41;	// 検索結果に表示したい文字数は？
 			const beforeLength = 15;	// 先読み、マッチした検索語句の何文字前から表示したい？
-			const afterLength = resultLength - beforeLength - searchWord.length;	// 後読み
+			const afterLength = resultLength - beforeLength - word.length;	// 後読み
 			
 			// マッチしたときは（本文・タイトル・タグのいずれかに）
-			if (searchWordIndex != -1 || searchWordIndex_title != -1 || searchWordIndex_hashtags != -1) {
+			if (wordIndex != -1 || isMatched_title === true || isMatched_hashtags === true) {
 				li.classList.remove('hp_hidden');	// 表示。
 				
-				// 検索語句が先頭に近すぎたら、
-				if (searchWordIndex <= beforeLength) {
-					searchWordIndex = beforeLength;	// 冒頭から表示して、
-					resultText = '';	// 冒頭の'…'を削除。
-				}				
-				// 結果表示用の文字列
-				resultText += eachData.plainText.substr(searchWordIndex - beforeLength, resultLength)
-				// 検索語句が末尾より十分遠ければ、
-				const searchWordIndex_last = searchWordIndex + searchWord.length + afterLength;
-				if (searchWordIndex_last < eachData.plainText.length) {
-					resultText += '…';	// 末尾に'…'を追加。
-				} 
+				// 検索結果に表示するための文字列を決定
+					// 検索語句が先頭に近すぎたら、
+					if (wordIndex <= beforeLength) {
+						wordIndex = beforeLength;	// 冒頭から表示して、
+						resultText = '';	// 冒頭の'…'を削除。
+					}				
+					// 結果表示用の文字列
+					resultText += eachData.plainText.substr(wordIndex - beforeLength, resultLength)
+					// 検索語句が末尾より十分遠ければ、
+					const wordIndex_last = wordIndex + word.length + afterLength;
+					if (wordIndex_last < eachData.plainText.length) {
+						resultText += '…';	// 末尾に'…'を追加。
+					}
+				
 				// 検索語句をハイライト表示する
-				resultText = resultText.replace(new RegExp(searchWord, "g"), `<span class="hp_highlight">${searchWord}<\/span>`);	// （変数を使って複数置換させる方法）
+				resultText = resultText.replace(new RegExp(word, "g"), `<span class="hp_highlight">${word}<\/span>`);	// （変数を使って複数置換させる方法）
 				// DOM要素として追加
 				li_text.innerHTML = `<p>${resultText}</p>`;
 				
@@ -184,9 +185,9 @@ fetch(jsonPath)
 			// マッチしなかったときは、
 				li.classList.add('hp_hidden');	// 非表示に。
 			}
-			
+
 			// 検索フォームが空になったら、
-			if (searchWord === '') {
+			if (word === '') {
 				li_text.innerHTML = eachData.postText	// 元のテキストに戻す。
 			};
 
@@ -254,7 +255,7 @@ fetch(jsonPath)
 	}
 	// ハッシュタグ、ハイライトされたとき
 	function html_hashtags_highlighted(eachTag) {
-		return `<li><a href="?tag=${eachTag}" class="hp_highlight">#${eachTag}</a></li>`	// リンクにタグフィルター用のクエリ文字列を仕込む
+		return `<li><a href="?tag=${eachTag}" class="hp_bold">#${eachTag}</a></li>`	// リンクにタグフィルター用のクエリ文字列を仕込む
 	}
 
 	// レンダラー（汎用）
