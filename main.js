1628c37e9387c0eed170a67ff78698824c967985
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-07-06 23:06:00 +0900

    ブログカード機能を実装。Web Componentsのカスタム要素を用いた

--- main.js
+++ main.js
@@ -27,8 +27,7 @@
 	
 	// 実行
 	const status = getUrlQueries();
-
-
+	
 /* JSONデータ取得開始 --------------------  */
 fetch(jsonPath)
  .then((response) => response.json())
@@ -198,7 +197,42 @@ fetch(jsonPath)
 		realTimeSearch();
 	});
 
+///----------------
+	class BlogCard extends HTMLElement {
+		constructor() {
+			super();
+			this.id = this.getAttribute('id');
+			this.i = data.length - this.id;
+			this.hashtags = data[this.i].tags
+					.map((eachTag) => `<li>#${eachTag}</li>`)
+					.join('');
+			this.innerHTML = `
+			<div class="bl_blogcard">
+				<a href="?id=${this.id}">
+					<header class="bl_blogcard_header">
+						<img src="assets/quotation-alpha.png" class="bl_blogcard_icon" width="20" height="20">
+						<div class="bl_blogcard_logo">placet experiri</span>
+						<span class="bl_blogcard_suffix"> :: ${this.id}</span>
+					</header>
+					<div class="bl_blogcard_title">${data[this.i].title}</div>
+					<p class="bl_blogcard_text">${data[this.i].plainText.substr(0, 57)}…</p>
+					<footer class="bl_blogcard_footer">
+						<span class="bl_blogcard_time">${moment(data[this.i].date).format("YYYY-MM-DD")}</span>
+						<ul class="bl_blogcard_tags">
+							${this.hashtags}
+						</ul>
+					</footer>
+				</a>
+			</div>`;
+		}
+	}
+
+	// レンダリングと結びつける
+	// 第一引数にHTML上のカスタムエレメント、第二引数にJS上のクラス名
+	window.customElements.define('blog-card', BlogCard);
 	
+//-------------------
+
 	/* ---------------------------------
 		テンプレート */		// 関数の巻き上げを使って上から参照する
 
