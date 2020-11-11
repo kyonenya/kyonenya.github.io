81c29807122dbf4b1937c69a04bb96427f90aa5b
kyonenya <kyon.musica@gmail.com>
2020-11-11 09:51:29 +0000

    メイン関数から余計な処理を切り離し、ルーティング部だけを残した

--- src/index.js
+++ src/index.js
@@ -8,36 +8,30 @@ import { registerComponents } from './component.js';
 
 const jsonPath = './data.json';
 
-  const app = (data) => {
-    // クエリを取得する
-    const status = getUrlQueries(window.location.search);
-
-    // ルーティング
-    if (status.id == null && status.tag) {
-        const postlist_tagged = createTagged(data, status.tag);
-        renderHTML(postlist_tagged);
-    } else if (status.id == null) {
-        const postlist = createPostlist(data); 
-        renderHTML(postlist);
-    }
-    if (isFinite(status.id)) {  // 数値判定
-        const i = data.length - status.id;  // 記事idはループカウントで言うと何番目か
-        const article = createArticle(data[i]);
-        renderHTML(article);  // ページ生成
-        document.querySelector('.el_search_form').classList.add('hp_hidden');  // 検索フォームを非表示
-    }
-
-    // リアルタイム検索
-    document.querySelector('.el_search_form').addEventListener('input', () => realTimeSearch(data));
+const route = (data, status) => {
+  if (status.id == null && status.tag) {
+      const postlist_tagged = createTagged(data, status.tag);
+      renderHTML(postlist_tagged);
+  } else if (status.id == null) {
+    const postlist = createPostlist(data); 
+      renderHTML(postlist);
   }
+  if (isFinite(status.id)) {  // 数値判定
+    const i = data.length - status.id;  // 記事idはループカウントで言うと何番目か
+    const article = createArticle(data[i]);
+    renderHTML(article);
+    document.querySelector('.el_search_form').classList.add('hp_hidden');  // 検索フォームを非表示
+  }
+}
 
 // JSONデータを取得して実行
-const index = async () => {
+const app = async () => {
   const response = await fetch(jsonPath);
   const data = await response.json();
   const posts = process(data);
-  app(posts);
+  route(posts, getUrlQueries(window.location.search));
+  document.querySelector('.el_search_form').addEventListener('input', () => realTimeSearch(data));  // リアルタイム検索
   registerComponents(posts);
 }
 
-index();
+app();
