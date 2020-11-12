c77c3c4c0ad0857d077ee874256660050fa64cc0
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-12 21:38:54 +0900

    for...ofをmapで書き換え

--- src/data.js
+++ src/data.js
@@ -1,26 +1,25 @@
 export const process = data => {
-  for (const eachData of data) {
-    // 1. ダブルダッシュ——が途切れてしまうので罫線二つに置換しておく
-    eachData.text = eachData.text.replace(/——/g, '──');
-    eachData.title = eachData.title.replace(/——/g, '──');
-
-    // 2. マークアップを削除してプレーンテキストを生成しておく
-    eachData.plainText = eachData.text.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '');
-
-    // 3. 記事一覧リストでの表示用文字列を作っておく
-    const postTextLength = 125; // 記事一覧に何文字表示するか？
-    // 長文なら、
-    if (eachData.plainText.length > postTextLength) {
-      eachData.postText = `${eachData.plainText.substr(0, postTextLength)}…`; // 冒頭n文字分だけを省略表示。
-    } else { // 短文なら、
-      eachData.postText = eachData.plainText; // プレーンテキストそのまま
-    }
-  }
-  
   for (let i = 0; i < data.length; i++) {
     data[i].index = i;
     data[i].id = data.length - i; // overwrite
   }
   
-  return data;
+  return data.map(aData => {
+    // ダブルダッシュ——が途切れてしまうので罫線二つに置換しておく
+    aData.text = aData.text.replace(/——/g, '──');
+    aData.title = aData.title.replace(/——/g, '──');
+    // マークアップを削除してプレーンテキストを生成しておく
+    aData.plainText = aData.text.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '');
+
+    // 記事一覧リストでの表示用文字列を作っておく
+    const postTextLength = 125; // 記事一覧に何文字表示するか？
+    // 長文なら、
+    if (aData.plainText.length > postTextLength) {
+      aData.postText = `${aData.plainText.substr(0, postTextLength)}…`; // 冒頭n文字分だけを省略表示。
+    } else { // 短文なら、
+      aData.postText = aData.plainText; // プレーンテキストそのまま
+    }
+    
+    return aData;
+  });
 };
