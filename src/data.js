158099cc6a4fcb4c544388212b9948c5a494306a
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-12 22:22:54 +0900

    シャローコピーをスプレッド演算子で取る

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 export const process = (data) => {
-  const result = data.slice();
+  const result = [ ...data ];
 
   for (let i = 0; i < result.length; i += 1) {
     result[i].index = i;
     result[i].id = result.length - i; // overwrite existing post id
   }
 
-  return result.map(aResult => {
+  return result.map((aResult) => {
     const aData = { ...aResult };
     aData.text = aData.text.replace(/——/g, '──'); // replace double-dash of ruled lines
     aData.title = aData.title.replace(/——/g, '──');
