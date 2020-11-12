a253feb7893d516c65eaebe4451dca255acdb0c8
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-12 20:45:53 +0900

    記事リスト生成のインライン化

--- src/model.js
+++ src/model.js
@@ -1,49 +1,39 @@
 import { template } from './template.js';
 
 export const createArticle = (data) => ({
-    html: template.article(data),
-    suffix: ` :: ${data.id}`,
-    description: `${data.plainText.substr(0, 110)}…`,
-    pageTitle: (data.title)
-      ? `${data.title}｜placet experiri :: ${data.id}`
-      : `placet experiri :: ${data.id}`,
-    archiveHeader: '',
-  });
+  html: template.article(data),
+  suffix: ` :: ${data.id}`,
+  description: `${data.plainText.substr(0, 110)}…`,
+  pageTitle: (data.title)
+    ? `${data.title}｜placet experiri :: ${data.id}`
+    : `placet experiri :: ${data.id}`,
+  archiveHeader: '',
+});
 
 export const createPostlist = (data) => ({
     html: `
-      <ul class="bl_posts">
-        ${data.map((aData) => {
+      <ul class="bl_posts">${
+        data.map((aData) => {
           return template.postList(data[data.length - aData.id], aData.id);
-        }).join('')}
-      </ul>`,
+        }).join('')
+      }</ul>`,
     suffix: '',
     description: '',
     pageTitle: '',
     archiveHeader: '',
   });
         
-export const createTagged = (data, tag) => {  
-  for (const eachData of data) {
-    // 6. 表示・非表示フラグを、タグフィルターに応じてセットしておく
-    const tagExists = eachData.tags.includes(tag);  // タグ検索にヒットしたか
-    // タグ検索がOFFか、またはタグ検索にヒットしているならば、
-    eachData.isVisible = tag == null || tagExists
-      ? true  // 表示。
-      : false;
-  }
-
-  return {
-    html: `<ul class="bl_posts">${
+export const createTagged = (data, tag) => ({
+  html: `
+    <ul class="bl_posts">${
       data.map((aData) => {
-        if (aData.isVisible === true) {
+        if (tag == null || aData.tags.includes(tag)) {
           return template.postList(data[data.length - aData.id], aData.id, tag);
         }
       }).join('')
-      }</ul>`,
-    suffix: '',
-    description: '',
-    pageTitle: `#${tag}｜placet experiri`,
-    archiveHeader: `#${tag}`,
-  }
-};
+    }</ul>`,
+  suffix: '',
+  description: '',
+  pageTitle: `#${tag}｜placet experiri`,
+  archiveHeader: `#${tag}`,
+});
