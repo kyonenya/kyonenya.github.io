769fe44188eeb07241048d054da7da733d97c9c1
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-12 17:07:31 +0900

    HTML生成をdata.jsからmodel.jsへ移行

--- src/model.js
+++ src/model.js
@@ -1,7 +1,14 @@
-import { html_article } from './template.js';
+import { html_postlist, html_article, html_hashtags, html_hashtags_highlighted } from './template.js';
 
 /* 個別記事ページ */
 export const createArticle = (post) =>  {
+  // 4. ハッシュタグを生成しておく
+  post.hashtags = post.tags
+    .map((eachTag) => {
+      return html_hashtags(eachTag);
+    })
+    .join('');
+  
   return {
     html: html_article(post),
     suffix: ` :: ${post.id}`,
@@ -14,6 +21,18 @@ export const createArticle = (post) =>  {
 }
 
 export const createPostlist = (data) => {
+  for (const eachData of data) {
+    // 4. ハッシュタグを生成しておく
+    eachData.hashtags = eachData.tags
+      .map((eachTag) => {
+          return html_hashtags(eachTag);
+      })
+      .join('');
+    // 5. 記事リストのHTMLを生成しておく
+    const i = data.length - eachData.id;
+    eachData.postlistHtml = html_postlist(data[i], eachData.id);
+  }
+  
   return {
     html: `<ul class="bl_posts">${
         data.map((eachData) => {
@@ -28,7 +47,23 @@ export const createPostlist = (data) => {
 }
         
 export const createTagged = (data, tag) => {  
-    for (const eachData of data) {
+  for (const eachData of data) {
+    // 4. ハッシュタグを生成しておく
+    eachData.hashtags = eachData.tags
+      .map((eachTag) => {
+        if (eachTag === tag) { // タグフィルターにマッチしているなら、
+          return html_hashtags_highlighted(eachTag); // 当該タグをハイライト。
+        } else {
+          return html_hashtags(eachTag);
+        }
+      })
+      .join('');
+    // 5. 記事リストのHTMLを生成しておく
+    const i = data.length - eachData.id;
+    eachData.postlistHtml = html_postlist(data[i], eachData.id);
+  }
+  
+  for (const eachData of data) {
       // 6. 表示・非表示フラグを、タグフィルターに応じてセットしておく
       const tagExists = eachData.tags.includes(tag);  // タグ検索にヒットしたか
       // タグ検索がOFFか、またはタグ検索にヒットしているならば、
