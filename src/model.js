adc32ead19e0bbb9d7248b15895d38f102571a1e
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-12 20:33:22 +0900

    記事リストHTML生成のインライン化

--- src/model.js
+++ src/model.js
@@ -1,50 +1,29 @@
 import { template } from './template.js';
 
-// const createHashtags = (tags, filtered = null) => tags.map(eachTag => {
-//     if (eachTag === filtered) { // タグフィルターにマッチしているなら、
-//       return template.filteredHashtag(eachTag); // 当該タグをハイライト。
-//     }
-//     return template.hashtag(eachTag);
-//   })
-//   .join('');
-
 export const createArticle = (data) => ({
     html: template.article(data),
     suffix: ` :: ${data.id}`,
     description: `${data.plainText.substr(0, 110)}…`,
     pageTitle: (data.title)
-      ? `${data.title}｜placet experiri :: ${data.id}`  // 記事タイトルあり
-      : `placet experiri :: ${data.id}`,  // 記事タイトルなし
+      ? `${data.title}｜placet experiri :: ${data.id}`
+      : `placet experiri :: ${data.id}`,
     archiveHeader: '',
   });
 
-export const createPostlist = (data) => {
-  for (const eachData of data) {
-    // 5. 記事リストのHTMLを生成しておく
-    const i = data.length - eachData.id;
-    eachData.postlistHtml = template.postList(data[i], eachData.id);
-  }
-  
-  return {
-    html: `<ul class="bl_posts">${
-      data.map((eachData) => {
-          return eachData.postlistHtml;
-      }).join('')
-      }</ul>`,
+export const createPostlist = (data) => ({
+    html: `
+      <ul class="bl_posts">
+        ${data.map((aData) => {
+          return template.postList(data[data.length - aData.id], aData.id);
+        }).join('')}
+      </ul>`,
     suffix: '',
     description: '',
     pageTitle: '',
     archiveHeader: '',
-  }
-};
+  });
         
 export const createTagged = (data, tag) => {  
-  for (const eachData of data) {
-    // 5. 記事リストのHTMLを生成しておく
-    const i = data.length - eachData.id;
-    eachData.postlistHtml = template.postList(data[i], eachData.id, tag);
-  }
-  
   for (const eachData of data) {
     // 6. 表示・非表示フラグを、タグフィルターに応じてセットしておく
     const tagExists = eachData.tags.includes(tag);  // タグ検索にヒットしたか
@@ -56,9 +35,9 @@ export const createTagged = (data, tag) => {
 
   return {
     html: `<ul class="bl_posts">${
-      data.map((eachData) => {
-        if (eachData.isVisible === true) {
-          return eachData.postlistHtml;
+      data.map((aData) => {
+        if (aData.isVisible === true) {
+          return template.postList(data[data.length - aData.id], aData.id, tag);
         }
       }).join('')
       }</ul>`,
