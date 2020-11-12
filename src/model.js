3ec217e0219b08dffa763b251393e6943e65acb3
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-12 17:21:57 +0900

    ハッシュタグ生成関数にハイライト機能を追加

--- src/model.js
+++ src/model.js
@@ -1,8 +1,14 @@
 import { html_postlist, html_article, html_hashtags, html_hashtags_highlighted } from './template.js';
 
-const createHashtags = (tags) => {
-  return tags.map(eachTag => html_hashtags(eachTag))
-    .join('');
+const createHashtags = (tags, filtered = null) => {
+  return tags.map((eachTag) => {
+    if (eachTag === filtered) { // タグフィルターにマッチしているなら、
+      return html_hashtags_highlighted(eachTag); // 当該タグをハイライト。
+    } else {
+      return html_hashtags(eachTag);
+    }
+  })
+  .join('');
 };
 
 /* 個別記事ページ */
@@ -18,11 +24,10 @@ export const createArticle = (data) =>  {
       : `placet experiri :: ${data.id}`,  // 記事タイトルなし
     archiveHeader: '',
   }
-}
+};
 
 export const createPostlist = (data) => {
   for (const eachData of data) {
-    // 4. ハッシュタグを生成しておく
     eachData.hashtags = createHashtags(eachData.tags);
     
     // 5. 記事リストのHTMLを生成しておく
@@ -41,45 +46,37 @@ export const createPostlist = (data) => {
     pageTitle: '',
     archiveHeader: '',
   }
-}
+};
         
 export const createTagged = (data, tag) => {  
   for (const eachData of data) {
-    // 4. ハッシュタグを生成しておく
-    eachData.hashtags = eachData.tags
-      .map((eachTag) => {
-        if (eachTag === tag) { // タグフィルターにマッチしているなら、
-          return html_hashtags_highlighted(eachTag); // 当該タグをハイライト。
-        } else {
-          return html_hashtags(eachTag);
-        }
-      })
-      .join('');
+    eachData.hashtags = createHashtags(eachData.tags, tag);
+    
     // 5. 記事リストのHTMLを生成しておく
     const i = data.length - eachData.id;
     eachData.postlistHtml = html_postlist(data[i], eachData.id);
   }
   
   for (const eachData of data) {
-      // 6. 表示・非表示フラグを、タグフィルターに応じてセットしておく
-      const tagExists = eachData.tags.includes(tag);  // タグ検索にヒットしたか
-      // タグ検索がOFFか、またはタグ検索にヒットしているならば、
-      eachData.isVisible = tag == null || tagExists
-        ? true  // 表示。
-        : false;
-    }
+    // 6. 表示・非表示フラグを、タグフィルターに応じてセットしておく
+    const tagExists = eachData.tags.includes(tag);  // タグ検索にヒットしたか
+    // タグ検索がOFFか、またはタグ検索にヒットしているならば、
+    eachData.isVisible = tag == null || tagExists
+      ? true  // 表示。
+      : false;
+  }
 
-    return {
-        html: `<ul class="bl_posts">${
-          data.map((eachData) => {
-              if (eachData.isVisible === true) {
-              return eachData.postlistHtml;
-              }
-          }).join('')
-          }</ul>`,
-        suffix: '',
-        description: '',
-        pageTitle: `#${tag}｜placet experiri`,
-        archiveHeader: `#${tag}`,
-    }
-}
+  return {
+    html: `<ul class="bl_posts">${
+      data.map((eachData) => {
+          if (eachData.isVisible === true) {
+          return eachData.postlistHtml;
+          }
+      }).join('')
+      }</ul>`,
+    suffix: '',
+    description: '',
+    pageTitle: `#${tag}｜placet experiri`,
+    archiveHeader: `#${tag}`,
+  }
+};
