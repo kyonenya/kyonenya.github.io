de5cab9e081744f0a5a1ee3a19ca6aeaa05ad37b
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-18 06:10:17 +0900

    検索関数と結果表示関数のフェーズを完全に分離

--- src/search.js
+++ src/search.js
@@ -1,29 +1,30 @@
 import { templates } from './templates.js';
 
-export const summaryFor = (aData, word) => {
-  const wordIndex = aData.plainText.indexOf(word);
+export const search = (aData, word) => {
   const resultLength = 50;
   const beforeLength = 20;
+  const afterLength = resultLength - beforeLength - word.length;
+  const wordIndex = aData.plainText.indexOf(word);
   const beforeIndex = wordIndex - beforeLength;
   const afterIndex = wordIndex + word.length;
-  const afterLength = resultLength - beforeLength - word.length;
-
+  const isMatched = wordIndex !== -1
+    || aData.title.includes(word)
+    || aData.tags.includes(word);
+  
   if (wordIndex === -1) {
-    return `${aData.plainText.substr(0, resultLength)}…`;
+    return [isMatched, `${aData.plainText.substr(0, resultLength)}…`];
   }
-
-  // 検索語句が先頭に近い場合
-  if (beforeIndex <= 0) {
-    return templates.searchedPost({
+  if (beforeIndex <= 0) { // マッチした語句が先頭に近い場合
+    return [isMatched, templates.searchedPost({
       beforeEllipsis: '',
       beforeText: aData.plainText.substr(0, wordIndex),
       word: aData.plainText.substr(wordIndex, word.length),
       afterText: aData.plainText.substr(afterIndex, resultLength - afterIndex),
       afterEllipsis: '…',
-    });
+    })];
   }
 
-  return templates.searchedPost({
+  return [isMatched, templates.searchedPost({
     beforeEllipsis: '…',
     beforeText: aData.plainText.substr(beforeIndex, beforeLength),
     word: aData.plainText.substr(wordIndex, word.length),
@@ -31,25 +32,23 @@ export const summaryFor = (aData, word) => {
     afterEllipsis: (beforeIndex + resultLength < aData.plainText.length)
       ? '…'
       : '',
-  });
+  })];
 };
 
 export const searchPosts = (data, word) => {
   data.forEach((aData) => {
     const postItemElement = document.querySelector(`.bl_posts_item[data-id="${aData.id}"]`);
     const summaryElement = document.querySelector(`.bl_posts_summary[data-id="${aData.id}"]`);
-    const wordIndex = aData.plainText.indexOf(word);
 
     if (!postItemElement) {
       return;
     }
 
-    if (wordIndex !== -1
-      || aData.title.includes(word)
-      || aData.tags.includes(word)
-    ) {
+    const [isMatched, summary] = search(aData, word);
+    
+    if (isMatched) {
       postItemElement.classList.remove('hp_hidden');
-      summaryElement.innerHTML = `<p>${summaryFor(aData, word, wordIndex)}</p>`;
+      summaryElement.innerHTML = `<p>${summary}</p>`;
     } else {
       postItemElement.classList.add('hp_hidden');
     }
