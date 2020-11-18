fb2f59a9d900d0bd077fa06265d0dba66a711a49
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-18 23:01:12 +0900

    検索：マッチしない場合の非表示を実装

--- src/templates.js
+++ src/templates.js
@@ -7,7 +7,7 @@ const filteredHashtag = aTag => `<li><a href="?tag=${aTag}" class="hp_bold">#${a
 
 const postList = (aData, filteredTag = null, searched = {}) => {
   return `
-  <li class="bl_posts_item" data-id=${aData.id}>
+  <li class="bl_posts_item${(searched.isMatched || false) ? '' : ' hp_hidden'}" data-id=${aData.id}>
     <a href="?id=${aData.id}">
       <header class="bl_posts_header">
         <time class="bl_posts_date" datetime="${dayjs(aData.date).format('YYYY-MM-DD HH:mm')}">${dayjs(aData.date).format('YYYY-MM-DD')}
@@ -18,7 +18,7 @@ const postList = (aData, filteredTag = null, searched = {}) => {
       </h2>
       <div class="bl_posts_summary" data-id=${aData.id}>
         <p>
-          ${searched.summary}
+          ${(!searched.isMatched) ? `${aData.plainText.substr(0, 125)}…` : searched.summary}
         </p>
       </div>
     </a>
