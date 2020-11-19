d51e4f954deab8b7acd435a9cff6dfeb6f5b8871
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-19 17:52:59 +0900

    dayjsをCDNではなくwebpack経由でインポート

--- src/templates.js
+++ src/templates.js
@@ -1,3 +1,8 @@
+// import dayjs from 'dayjs';
+// import 'dayjs/locale/ja';
+// import relativeTime from 'dayjs/plugin/relativeTime';
+// dayjs.extend(relativeTime);
+
 dayjs.locale('ja');
 dayjs.extend(dayjs_plugin_relativeTime);
 
