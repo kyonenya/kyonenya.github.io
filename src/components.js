37cd0ede35475f71a3ae5f22854c7a6a207ababb
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-11-21 14:22:06 +0900

    バグ修正：ブログカードのdayjsインポート

--- src/components.js
+++ src/components.js
@@ -1,3 +1,8 @@
+// import dayjs from 'dayjs';
+// import 'dayjs/locale/ja';
+
+// dayjs.locale('ja');
+
 export const registerComponents = (data) => {
   class BlogCard extends HTMLElement {
     constructor() {
