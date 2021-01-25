5b7d4e009a66c1671dff8205c652e4bc5bf8d257
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-25 23:26:47 +0900

    fix: 新着バッジを非表示に

--- src/notify.ts
+++ src/notify.ts
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/ja';
 
 // modified at
-const dateStr = '2021-01-09';
+const dateStr = '2020-12-05';
 
 export const notifyUpdate = (): void => {
   const limitDate = dayjs(dateStr).add(1, 'month');
