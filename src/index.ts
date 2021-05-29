7b074f102bc264d373b55d198b251be3455086ee
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-29 21:33:43 +0900

    refactor: エントリーポイントの整理・定数を関数外に出す

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,40 @@
 import { enrich } from './data';
 import { route } from './router';
 import { registerComponents } from './components';
+import { fetcher } from './utils';
 import { notifyUpdate } from './notify';
 import { datarable } from './types';
 
 const jsonPath = './data.json';
+const searchFormElement = <HTMLFormElement>(
+  document.querySelector('.el_search_form')
+);
+const searchInputElement = <HTMLInputElement>(
+  document.querySelector('.el_search_input')
+);
 
-const fetcher = async <T>(url: string): Promise<T> => {
-  const response = await fetch(url);
-  if (!response.ok) throw new Error(response.statusText);
-  return (await response.json()) as T;
+const bootstrap = (data: datarable[]): void => {
+  route(data);
+  window.addEventListener('popstate', () => route(data));
+
+  searchFormElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    window.history.pushState(
+      `${window.location.search}#${searchInputElement.value}`,
+      '',
+      `${window.location.search}#${searchInputElement.value}`
+    );
+    route(data);
+  });
+
+  notifyUpdate();
+  registerComponents(data);
 };
 
 const index = async (): Promise<void> => {
   const rawData = await fetcher<datarable[]>(jsonPath);
   const data = enrich(rawData);
-
-  const formElement = <HTMLFormElement>(
-    document.querySelector('.el_search_form')
-  );
-  const inputElement = <HTMLInputElement>(
-    document.querySelector('.el_search_input')
-  );
-  formElement.addEventListener('submit', (e) => {
-    e.preventDefault();
-    window.history.pushState(
-      null,
-      '',
-      `${window.location.search}#${inputElement.value}`
-    );
-    route(data);
-  });
-  notifyUpdate();
-
-  route(data);
-  window.addEventListener('popstate', () => route(data));
-  registerComponents(data);
+  bootstrap(data);
 };
 
 index().catch((err) => console.error(err));
---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+export const fetcher = async <T>(url: string): Promise<T> => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(response.statusText);
+  return (await response.json()) as T;
+};
