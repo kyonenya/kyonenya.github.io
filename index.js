884327a7f7711e7b6a3cbe262eac60dc439d7889
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-05-29 20:30:07 +0900

    chore: update sitemap

--- index.js
+++ index.js
@@ -10,13 +10,9 @@ const middleware = webpackDevMiddleware(webpack(config), {
   publicPath: config.output.publicPath,
 });
 
-const app = express();
-app
+express()
   .use(middleware)
   .get('/', (req, res) => res.sendFile(path.resolve(rootDir, 'index_dev.html')))
   .use(express.static(rootDir))
   .listen(port, () => console.log(`Launching app... http://localhost:${port}\n`))
   ;
-
-// Register app and middleware. Required for better performance when running from play.js
-// pjs.register(app, middleware);
