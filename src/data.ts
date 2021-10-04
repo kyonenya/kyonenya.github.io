26fce8903d2aa7641f79a6144ceff630b87150f3
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-10-04 23:15:34 +0900

    improve: 全角スペースを半角スペースに置換

--- src/data.ts
+++ src/data.ts
@@ -5,7 +5,8 @@ export const enrich = (data: datarable[]): datarable[] =>
     const aData = { ..._aData };
     aData.id = data.length - idx;
     aData.index = idx;
-    aData.text = aData.text.replace(/——/g, '──'); // replace double-dash of ruled lines
+    aData.text = aData.text.replace(/——/g, '──'); // ダブルダッシュ -> 罫線2つ
+    aData.text = aData.text.replace(/　/g, ' '); // 全角スペース -> 半角スペース
     aData.title = aData.title.replace(/——/g, '──');
     aData.plainText = aData.text.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '');
     return aData;
