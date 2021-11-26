892f4e0cbf32cb6b208da96745d021ac3152a1d6
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-11-26 10:09:47 +0900

    feat: 文献タイプ判別

--- src/works/bibliography.ts
+++ src/works/bibliography.ts
@@ -2,21 +2,40 @@ import { Data } from 'csl-json';
 
 export type Bibliography = {
   text: string;
-  isTranslation: boolean;
-  data: Data;
+  type: BibType | undefined;
+  item: Data;
 };
+
+export type BibType = '論文' | '発表' | '翻訳' | '書籍';
+
+function detectBibType(item: Data): BibType | undefined {
+  switch (item.type) {
+    case 'article-journal':
+      if (item.translator) return '翻訳';
+      return '論文';
+      break;
+    case 'paper-conference':
+      return '発表';
+      break;
+    case 'book':
+      return '書籍';
+      break;
+    default:
+      return undefined;
+  }
+}
 
 export function toBibliographies(
   texts: string[],
   items: Data[]
 ): Bibliography[] {
   return texts.map((text, i) => {
-    const data = items[i];
+    const item = items[i];
 
     return {
       text,
-      isTranslation: !!data.translator,
-      data,
+      type: detectBibType(item),
+      item,
     };
   });
 }
