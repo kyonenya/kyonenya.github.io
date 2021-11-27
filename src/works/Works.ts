c73430e9ea35922dc9c48ff2411854a2f883a11e
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-11-27 16:27:19 +0900

    feat: マークダウンリンクを解釈

--- src/works/Works.ts
+++ src/works/Works.ts
@@ -1,6 +1,14 @@
 import { Bibliography, BibliographyMap, Category } from './bibliography';
 
-const Item = (text: string) => `<li>${text}</li>`;
+const Text = (text: string) =>
+  text.replace(
+    /\[(.+?)\]\((.+?)\)/g,
+    (_match, title, url) => `<a href="${url}">${title}</a>`
+  );
+
+const Item = (text: string) => `<li>${Text(text)}</li>`;
+
+const HilightedItem = (text: string) => `<string>${Item(text)}</strong>`;
 
 const List = (bibs: Bibliography[] | undefined, category: Category): string => {
   if (!bibs || bibs.length === 0) return '';
