6e0fa80956324ef03e8b178388cd1b97f2c1e33d
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-11-30 11:59:05 +0900

    refactor: parseInt

--- src/components.ts
+++ src/components.ts
@@ -6,6 +6,7 @@ export const registerComponents = (data: datarable[]): void => {
     constructor() {
       super();
       this.id = this.getAttribute('id')!;
+      const id = parseInt(this.id, 10);
       this.innerHTML = `
         <div class="bl_blogcard">
           <a href="?id=${this.id}">
@@ -15,22 +16,17 @@ export const registerComponents = (data: datarable[]): void => {
               <span class="bl_blogcard_suffix"> :: ${this.id}</span>
             </header>
             <div class="bl_blogcard_title">
-              ${data[data.length - parseInt(this.id, 10)].title}
+              ${data[data.length - id].title}
             </div>
             <p class="bl_blogcard_text">
-              ${data[data.length - parseInt(this.id, 10)].plainText.substr(
-                0,
-                56
-              )}…
+              ${data[data.length - id].plainText.substr(0, 56)}…
             </p>
             <footer class="bl_blogcard_footer">
               <span class="bl_blogcard_time">
-                ${dayjs(data[data.length - parseInt(this.id, 10)].date).format(
-                  'YYYY-MM-DD'
-                )}
+                ${dayjs(data[data.length - id].date).format('YYYY-MM-DD')}
               </span>
               <ul class="bl_blogcard_tags">
-                ${data[data.length - parseInt(this.id, 10)].tags
+                ${data[data.length - id].tags
                   .map((eachTag) => `<li>#${eachTag}</li>`)
                   .join('')}
               </ul>
