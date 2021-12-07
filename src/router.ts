26ef0b0c494bc77fb566c199084ccd49404edbad
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-12-07 21:46:08 +0900

    refactor: ルーティング定義とルート実行を同じモジュールに

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,55 @@
+import { Article } from './Article';
+import { PostList, PostListTagged, PostListSearched } from './PostList';
+import { renderPage } from './lib/render';
 import { Post } from './post';
-import { routes } from './routes';
 import { toState } from './state';
+
+const searchInputElement = document.querySelector('.el_search_input');
+
+const routes = {
+  article: (post: Post): void => {
+    searchInputElement?.classList.add('hp_hidden'); // disable search form
+    return renderPage({
+      body: Article(post),
+      suffix: ` :: ${post.id}`,
+      description: `${post.plainText.substr(0, 110)}…`,
+      title: post.title
+        ? `${post.title}｜placet experiri :: ${post.id}`
+        : `placet experiri :: ${post.id}`,
+      archiveHeader: '',
+    });
+  },
+  postList: (posts: Post[]): void =>
+    renderPage({
+      body: PostList(posts),
+      suffix: '',
+      description: '',
+      title: 'placet experiri',
+      archiveHeader: '',
+    }),
+  taggedPostList: (posts: Post[], tag: string): void =>
+    renderPage({
+      body: PostListTagged(posts, tag),
+      suffix: '',
+      description: '',
+      title: `#${tag}｜placet experiri`,
+      archiveHeader: `#${tag}`,
+    }),
+  searchedPostList: (posts: Post[], keyword: string, tag?: string): void =>
+    renderPage({
+      body: PostListSearched(posts, keyword, tag ?? null),
+      suffix: '',
+      description: '',
+      title: `「${keyword}」｜placet experiri`,
+      archiveHeader: `「${keyword}」`,
+    }),
+  beforeEach: (): void => {
+    window.scrollTo(0, 0);
+    searchInputElement?.classList.remove('hp_hidden');
+  },
+};
+
+// export type Route = keyof typeof routes;
 
 export function route(posts: Post[]): void {
   const { id, tag, keyword } = toState(
