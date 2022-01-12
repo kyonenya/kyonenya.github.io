6cd553aedf2678231b34b20aa11debb3305a77a8
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2022-01-12 22:46:32 +0900

    refactor: remove post id validation

--- src/post.ts
+++ src/post.ts
@@ -23,22 +23,16 @@ export type JSONPost = {
 export const jsonToPost = (posts: JSONPost[]): Post[] =>
   posts
     .filter((post) => dayjs(post.createdAt).isBefore(dayjs())) // exclude reserved post
-    .map((post, i) => {
-      if (post.id !== posts.length - i) {
-        // throw new Error('ID should be sequential.');
-      }
-      return {
-        ...post,
-        index: i,
-        title:
-          post.title === null || post.title === '' ? undefined : post.title,
-        text: post.text
-          .replaceAll('——', '──') // double dash -> double ruled line
-          .replaceAll('　', ' ') // full-width space -> half-width space
-          .replaceAll(
-            /<a (href='[^?].+?'.*?)>(.+?)<\/a>/g, // overwrite external link
-            "<a $1 target='_blank' rel='noopener'>$2</a>"
-          ),
-        plainText: post.text.replaceAll(/<("[^"]*"|'[^']*'|[^'">])*>/g, ''),
-      };
-    });
+    .map((post, i) => ({
+      ...post,
+      index: i,
+      title: post.title === null || post.title === '' ? undefined : post.title,
+      text: post.text
+        .replaceAll('——', '──') // double dash -> double ruled line
+        .replaceAll('　', ' ') // full-width space -> half-width space
+        .replaceAll(
+          /<a (href='[^?].+?'.*?)>(.+?)<\/a>/g, // overwrite external link
+          "<a $1 target='_blank' rel='noopener'>$2</a>"
+        ),
+      plainText: post.text.replaceAll(/<("[^"]*"|'[^']*'|[^'">])*>/g, ''),
+    }));
