48f381f941ae7fcc20a97f591e2e4a580699becf
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2022-01-15 11:05:15 +0900

    refactor: typeof keyof Unit

--- src/lib/date-const.ts
+++ src/lib/date-const.ts
@@ -1,13 +1,3 @@
-export const Unit = [
-  'second',
-  'minute',
-  'hour',
-  'day',
-  'month',
-  'year',
-] as const;
-export type Unit = typeof Unit[number];
-
 const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
@@ -15,7 +5,7 @@ const day = hour * 24;
 const month = day * 30;
 const year = month * 12;
 
-const msMap: { [k in Unit]: number } = {
+const msMap = {
   second,
   minute,
   hour,
@@ -23,6 +13,9 @@ const msMap: { [k in Unit]: number } = {
   month,
   year,
 };
+
+export const Unit = Object.keys(msMap) as (keyof typeof msMap)[];
+export type Unit = typeof Unit[number];
 
 export function toUnitTime(ms: number, unit: Unit): number {
   return ms / msMap[unit];
