9f1c922c9f591923979144a00a754e7704142adb
kyonenya <62150154+kyonenya@users.noreply.github.com>
2022-02-02 23:32:11 +0900

    feat: お知らせを追加 (#66)
    * fix: ブログカードの省略文字の位置ズレ

--- generateSitemap.js
+++ generateSitemap.js
@@ -4,7 +4,7 @@ const format = require('xml-formatter');
 const posts = require('./posts.json');
 
 const worksLastmod = '2021-11-28';
-const aboutLastmod = '2021-11-28';
+const aboutLastmod = '2022-02-02';
 
 const sitemap = new SitemapStream({ hostname: 'https://kyonenya.github.io/' });
 
