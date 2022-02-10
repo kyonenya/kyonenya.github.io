5047b03355dfbbc4af451f5f6d91a4b0e3e833fa
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2022-02-10 12:30:38 +0900

    chore: 開発用htmlが不要に

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ const port = process.env['WEB_APP_PORT'] ? process.env['WEB_APP_PORT'] : 3100;
 
 express()
   .use(webpackDevMiddleware(webpack(config)))
-  .get('/', (req, res) => res.sendFile(path.resolve(rootDir, 'index_dev.html')))
+  .get('/', (req, res) => res.sendFile(path.resolve(rootDir, 'index.html')))
   .get('/works', (req, res) =>
-    res.sendFile(path.resolve(rootDir, 'works_dev.html'))
+    res.sendFile(path.resolve(rootDir, 'works.html'))
   )
+  .get('/about', (req, res) =>
+    res.sendFile(path.resolve(rootDir, 'about.html'))
+  )
+  // redirect production -> development
+  .get('/dist/:scriptName', (req, res) =>
+    res.redirect(`/dist/dev/${req.params.scriptName}`)
+  )
+  .get('/dist/css/bundle.css', (req, res) => res.redirect('/src/css/index.css'))
   .use(express.static(rootDir))
   .listen(port, () =>
     console.log(`Launching app... http://localhost:${port}\n`)
