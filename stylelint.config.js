6ef1588361d13003fea9ca7a661f4046cefab408
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2022-02-10 11:39:21 +0900

    chore: ignore stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'comment-empty-line-before': null, // Expected empty line before comment
+  },
 };
