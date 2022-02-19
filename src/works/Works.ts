09ad196473b028f468a384067aa4979746abd30b
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2022-02-19 15:05:42 +0900

    refactor: citationが新しいかどうかはリストアイテムコンポーネントが知っているべき

--- src/works/Works.ts
+++ src/works/Works.ts
@@ -5,25 +5,20 @@ import { isNewCitation } from './citationDate';
 
 const Text = (text: string): string => MarkupText(parseMarkdownLink(text));
 
-const StrongText = (text: string) =>
-  `<span class='hp_bold'>${Text(text)}</span>`;
+const BoldText = (text: string) => `<span class='hp_bold'>${Text(text)}</span>`;
 
-const Item = (text: string) => `<li>${Text(text)}</li>`;
-
-const HilightedItem = (text: string): string => `<li>${StrongText(text)}</li>`;
+const ListItem = (citation: Citation) => `
+  <li>
+    ${(isNewCitation(citation) ? BoldText : Text)(citation._bibliographyText)}
+  </li>
+`;
 
 const List = (citations: Citation[] | undefined, genre: Genre): string => {
   if (!citations || citations.length === 0) return '';
   return `
     <h3>${genre}</h3>
     <ol>
-      ${citations
-        .map((citation) =>
-          (isNewCitation(citation) ? HilightedItem : Item)(
-            citation._bibliographyText
-          )
-        )
-        .join('')}
+      ${citations.map((citation) => ListItem(citation)).join('')}
     </ol>`;
 };
 
