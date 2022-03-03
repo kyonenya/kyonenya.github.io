0522daf326d50f99e2eb506c49447039b513a82f
kyonenya <62150154+kyonenya@users.noreply.github.com>
2022-03-03 12:40:07 +0900

    improve: YouTube埋め込みのスタイリング (#76)
    * improve: ダブルダッシュを長く（罫線置換に戻す）

--- src/lib/MarkupText.ts
+++ src/lib/MarkupText.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-irregular-whitespace */
 export const MarkupText = (html: string): string =>
   html
-    // kerning of double dash
-    .replaceAll('——', '<span class="hp_kerning">——</span>')
+    // double dash -> double ruled line
+    .replaceAll('——', '──')
     // full-width space -> half-width space after (！|？)
     .replaceAll(/([！？])　/g, (_, token: string) => `${token} `)
-    // unset paragraph indent start with '「'
+    // unset paragraph indent start with brackets
     .replaceAll(
       /<p>([「『（].+?)<\/p>/g,
       (_, content: string) => `<p style="text-indent: 0">${content}</p>`
