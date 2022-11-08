fa0def180532a63f9ecf9f7c107279b90a2959e1
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2022-11-08 15:06:54 +0900

    feat: 一件追記してjsonに書き出し

--- generatePosts.js
+++ generatePosts.js
@@ -4,6 +4,9 @@ const MarkdownIt = require('markdown-it');
 const MarkdownItFootnote = require('markdown-it-footnote');
 const matter = require('gray-matter');
 
+const jsonPath = path.resolve(__dirname, 'posts.json');
+const posts = require(jsonPath);
+
 const md = new MarkdownIt();
 md.use(MarkdownItFootnote);
 
@@ -11,7 +14,18 @@ const postPath = path.resolve('posts', '41.md');
 const post = fs.readFileSync(postPath, 'utf-8');
 
 const postMatter = matter(post);
-console.log(postMatter);
 
-const html = md.render(postMatter.content);
-console.log(html);
+const html = md.render(postMatter.content).replace(/\n/g, '');
+
+const appendedPost = {
+  ...postMatter.data,
+  text: html,
+};
+
+const newPosts = [appendedPost, ...posts];
+const uniquePosts = Array.from(
+  new Map(newPosts.map((post) => [post.id, post])).values()
+);
+//console.log(uniquePosts);
+
+fs.writeFileSync(jsonPath, JSON.stringify(uniquePosts));
