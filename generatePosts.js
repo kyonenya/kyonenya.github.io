3707752bb17f490a5ad6a48392061bfa3d6c449d
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2022-11-09 15:03:50 +0900

    feat: 完成。フォルダ内のmdをすべてjsonに追記

--- generatePosts.js
+++ generatePosts.js
@@ -4,28 +4,64 @@ const MarkdownIt = require('markdown-it');
 const MarkdownItFootnote = require('markdown-it-footnote');
 const matter = require('gray-matter');
 
-const jsonPath = path.resolve(__dirname, 'posts.json');
-const posts = require(jsonPath);
-
 const md = new MarkdownIt();
 md.use(MarkdownItFootnote);
 
-const postPath = path.resolve('posts', '41.md');
-const post = fs.readFileSync(postPath, 'utf-8');
+const jsonPath = path.resolve(__dirname, 'posts.json');
+const mdPath = path.resolve(__dirname, 'posts');
 
-const postMatter = matter(post);
+/**
+ * @param dir string
+ * @return string[]
+ */
+const listFiles = (dir) =>
+  fs
+    .readdirSync(dir, { withFileTypes: true })
+    .flatMap((dirent) =>
+      dirent.isFile()
+        ? [`${dir}/${dirent.name}`]
+        : listFiles(`${dir}/${dirent.name}`)
+    );
 
-const html = md.render(postMatter.content).replace(/\n/g, '');
+/**
+ * @param paths string[]
+ * @return {import('./src/post').JSONPost[]}
+ */
+function readPostsMarkdown(paths) {
+  return paths
+    .map((path) => fs.readFileSync(path, 'utf-8'))
+    .map((string) => matter(string))
+    .map((matter) => ({
+      ...matter.data,
+      text: md.render(matter.content).replace(/\n/g, ''),
+    }));
+}
 
-const appendedPost = {
-  ...postMatter.data,
-  text: html,
-};
+/**
+ * @param posts {import('./src/post').JSONPost[]}
+ * @return {import('./src/post').JSONPost[]}
+ */
+function uniquePosts(posts) {
+  const uniquePosts = Array.from(
+    new Map(posts.map((post) => [post.id, post])).values()
+  );
+  return uniquePosts.sort(function (a, b) {
+    if (a.id < b.id) return 1;
+    if (a.id > b.id) return -1;
+    return 0;
+  });
+}
 
-const newPosts = [appendedPost, ...posts];
-const uniquePosts = Array.from(
-  new Map(newPosts.map((post) => [post.id, post])).values()
-);
-//console.log(uniquePosts);
+/**
+ * @param posts {import('./src/post').JSONPost[]}
+ * @param mdPosts {import('./src/post').JSONPost[]}
+ * @return {void}
+ */
+function generatePosts(posts, mdPosts) {
+  fs.writeFileSync(
+    jsonPath,
+    JSON.stringify(uniquePosts([...mdPosts, ...posts]))
+  );
+}
 
-fs.writeFileSync(jsonPath, JSON.stringify(uniquePosts));
+generatePosts(readPostsMarkdown(listFiles(mdPath)), require(jsonPath));
