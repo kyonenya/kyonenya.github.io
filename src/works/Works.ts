e5cae0cdf2a9b68aff1e1b0de1973465bd373a23
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2025-05-21 04:25:51 +0900

    fix: eslint error

--- src/works/Works.ts
+++ src/works/Works.ts
@@ -4,7 +4,7 @@ import { isNewCitation } from './citationDate';
 
 const Text = (text: string): string =>
   parseMarkdownLink(
-    text.replace(/——(?![^\(]*\))/g, '<span class="hp_kerning">——</span>') // kerning except link href
+    text.replace(/——(?![^(]*\))/g, '<span class="hp_kerning">——</span>') // kerning except link href
   );
 
 const BoldText = (text: string) => `<b>${Text(text)}</b>`;
