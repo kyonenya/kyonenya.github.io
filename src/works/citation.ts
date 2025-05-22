cd865e9e5884f650241b5517bad56046534d952d
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2025-05-23 03:52:37 +0900

    improve: 書籍情報のjsonパース、表示の改善

--- src/works/citation.ts
+++ src/works/citation.ts
@@ -5,20 +5,20 @@ export type Citation = Data & {
   _bibliographyText: string;
 };
 
-export const Genre = ['論文', '発表', '翻訳', '書籍'] as const;
+export const Genre = ['書籍', '論文', '発表', '翻訳'] as const;
 export type Genre = typeof Genre[number];
 
 function detectGenre(item: Citation): Genre | undefined {
   switch (item.type) {
+    case 'book':
+      return '書籍';
+      break;
     case 'article-journal':
       if (item.translator) return '翻訳';
       return '論文';
       break;
     case 'paper-conference':
       return '発表';
-      break;
-    case 'book':
-      return '書籍';
       break;
     default:
       return undefined;
