69fe91b3fc8811bc024ef1094fb85a0a68d1a293
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2026-05-11 13:32:23 +0900

    refactor: 'property-no-vendor-prefix', 'declaration-property-value-keyword-no-deprecated'

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,14 +3,12 @@ export default {
   extends: ['stylelint-config-standard'],
   rules: {
     'comment-empty-line-before': null,
+    'selector-class-pattern': null, // allow PRECSS style
     // disabled for config-standard@39
-    'selector-class-pattern': null,
     'color-function-notation': null,
     'hue-degree-notation': null,
     'color-function-alias-notation': null,
     'alpha-value-notation': null,
     'media-feature-range-notation': null,
-    'property-no-vendor-prefix': null,
-    'declaration-property-value-keyword-no-deprecated': null,
   },
 };
