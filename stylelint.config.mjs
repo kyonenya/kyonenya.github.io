0e0da338756752322334bf147657c0339d754b91
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2026-05-11 13:34:17 +0900

    refactor: 'color-function-notation'

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,7 +5,6 @@ export default {
     'comment-empty-line-before': null,
     'selector-class-pattern': null, // allow PRECSS style
     // disabled for config-standard@39
-    'color-function-notation': null,
     'hue-degree-notation': null,
     'color-function-alias-notation': null,
     'alpha-value-notation': null,
