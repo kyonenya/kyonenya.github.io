eb3abe9d0f90161dd64b3dd76ee4685a69a3c1d3
kyonenya <kyonenya.takashi@gmail.com>
2026-05-18 07:53:57 +0900

    refactor: CSSのlintルール適用 (#103)
    * improve: ライトモードの文字色を濃く
    * refactor: lint-css

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -3,14 +3,8 @@ export default {
   extends: ['stylelint-config-standard'],
   rules: {
     'comment-empty-line-before': null,
-    // disabled for config-standard@39
-    'selector-class-pattern': null,
-    'color-function-notation': null,
-    'hue-degree-notation': null,
-    'color-function-alias-notation': null,
-    'alpha-value-notation': null,
-    'media-feature-range-notation': null,
-    'property-no-vendor-prefix': null,
-    'declaration-property-value-keyword-no-deprecated': null,
+    'selector-class-pattern': null, // allow PRECSS style
+    'hue-degree-notation': 'number',
+    'media-feature-range-notation': 'prefix', // compat for postcss-csso
   },
 };
